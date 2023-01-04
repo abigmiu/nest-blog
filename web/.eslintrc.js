e13885d6e71230289ef766ee3b617917b968e575
abigmiu <suraelax@gmail.com>
2023-01-04 17:59:24 +0800

    init nuxt web

---
+++ web/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    parser: 'vue-eslint-parser',
+
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'semi': ['error', 'always'],
+        // "indent": ["error", 4],
+        "quotes": ['error', 'single'],
+        '@typescript-eslint/no-unused-vars': ['error', {
+            vars: 'all',
+            args: 'none',
+            ignoreRestSiblings: true,
+        }],
+
+    },
+};
